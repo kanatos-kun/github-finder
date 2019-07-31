814894997438cc54837a3381a48f51ae5472d04c
kanatos <geoffreydu89@gmail.com>
2019-07-31 18:43:34 +0200

    add spinner loading

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,14 +1,19 @@
 import React, { Component } from "react";
 import NavBar from "./components/layout/NavBar";
 import "./App.css";
+import Search from "./components/users/Search";
 import Users from "./components/users/Users";
+
 import axios from "axios";
 class App extends Component {
   state = { users: [], loading: false };
   async componentDidMount() {
     this.setState({ loading: true });
-    const res = await axios.get("https://api.github.com/users");
-    console.log(res.data);
+    const res = await axios.get(
+      `https://api.github.com/users?
+      client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&
+      client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
     this.setState({ users: res.data, loading: false });
   }
 
@@ -17,6 +22,7 @@ class App extends Component {
       <div className="App">
         <NavBar />
         <div className="container">
+          <Search />
           <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </div>
