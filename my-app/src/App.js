3484093e3c18ec11a31a244835d2d7a78ac5bc91
kanatos <geoffreydu89@gmail.com>
2019-11-13 15:58:50 +0100

    34.Home & NotFound Components

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,9 @@
-import React, { Fragment } from "react";
+import React from "react";
 import NavBar from "./components/layout/NavBar";
-import Search from "./components/users/Search";
-import Users from "./components/users/Users";
 import User from "./components/users/User";
 import Alert from "./components/layout/Alert";
+import Home from "./components/pages/Home";
+import NotFound from "./components/pages/NotFound";
 import About from "./components/pages/About";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
@@ -21,18 +21,10 @@ const App = () => {
             <div className="container">
               <Alert />
               <Switch>
-                <Route
-                  exact
-                  path="/"
-                  render={props => (
-                    <Fragment>
-                      <Search />
-                      <Users />
-                    </Fragment>
-                  )}
-                />
+                <Route exact path="/" component={Home} />
                 <Route exact path="/about" component={About} />
                 <Route exact path="/user/:login" component={User} />
+                <Route component={NotFound} />
               </Switch>
             </div>
           </div>
